958e2c00fb1f1d4be6997024d9df444037297f91
Marco <marcoross37@gmail.com>
2020-07-25 21:37:26 +0200

    Fixed the DoublyLinkedList and the DoubleStackArray.

--- src/DoubleStackArray.java
+++ src/DoubleStackArray.java
@@ -1,61 +1,82 @@
 public class DoubleStackArray {
-    private int[] data;
+    private int[] Arr;
     private int top = -1;
-    private static final int capacity = 2;
+    private int bottomSize;
 
-    DoubleStackArray(int capacity){
-        data = new int [capacity];
+    DoubleStackArray(int capacity) {
+        Arr = new int[capacity];
+        bottomSize = capacity; //Set bottom of stack to length of list
     }
 
-    DoubleStackArray(){
-        this(capacity);
+    public int topSize() {
+        return (top + 1); //top is the index values for the list, and topSize returns the length.
     }
 
-    public int size() {
-        return (top + 1);
-    }
-
-    public void push(int value) throws IllegalStateException {
-        if (size() == data.length)
+    public void pushStackOne(int value) throws IllegalStateException {
+        if (topSize() == bottomSize) //If the length of the left(top) values equal the right(bottom) size, that means the stacks have collided and cannot add anymore to either.
             throw new IllegalStateException("StackOverflowException");
+
+        //Move the value onto the top of the stack and add one
         top++;
-        data[top] = value;
+        Arr[top] = value;
     }
 
-    public void push2(int value) throws IllegalStateException {
-        if (size() == data.length)
+    public void pushStackTwo(int value) throws IllegalStateException {
+        if (topSize() == bottomSize) //If the length of the left(top) values equal the right(bottom) size, that means the stacks have collided and cannot add anymore to either.
             throw new IllegalStateException("StackOverflowException");
-        top++;
-        data[top] = value;
+
+        //Move the value onto the bottom of the stack and subtract one
+        bottomSize--;
+        Arr[bottomSize] = value;
     }
 
-    public int pop( ) {
-        if (isEmpty( ))
+    public int popStackOne() {
+        if (isStackOneEmpty())
             throw new IllegalStateException("StackEmptyException");
-        int topVal = data[top];
+
+        //Removes one from the right of list, therefore it subtracts from top
+        int topVal = Arr[top];
         top--;
         return topVal;
     }
-    public boolean isEmpty(){
+
+    public int popStackTwo() {
+        if (isStackTwoEmpty())
+            throw new IllegalStateException("StackEmptyException");
+
+        //Removes one from the right(bottom) of list, therefore it adds to bottom
+        int bottomVal = Arr[bottomSize];
+        bottomSize++;
+        return bottomVal;
+    }
+
+    public boolean isStackOneEmpty() {
         return (top == -1);
     }
 
-    public int top( ) throws IllegalStateException {
-        if (isEmpty( ))
-            throw new IllegalStateException("StackEmptyException");
-        return data[top];
+    public boolean isStackTwoEmpty() {
+        return (bottomSize == Arr.length);
     }
-
 
     public static void main(String[] args) {
 
-        DoubleStackArray doubleStackArray = new DoubleStackArray();
+        DoubleStackArray doubleStackArray = new DoubleStackArray(10);
 
-        doubleStackArray.push(1);
+        //Stack 1 from the front of array
+        doubleStackArray.pushStackOne(1);
+        doubleStackArray.pushStackOne(3);
+        doubleStackArray.pushStackOne(5);
+        doubleStackArray.pushStackOne(7);
 
-        doubleStackArray.push2(2); //Stack 2 from the back of array
+        //Stack 2 from the back of array
+        doubleStackArray.pushStackTwo(2);
+        doubleStackArray.pushStackTwo(4);
+        doubleStackArray.pushStackTwo(6);
+        doubleStackArray.pushStackTwo(8);
+        doubleStackArray.pushStackTwo(10);
+        doubleStackArray.pushStackTwo(12);
 
-
-
+        System.out.println("First stack " + doubleStackArray.popStackOne());
+        System.out.println("Second stack " + doubleStackArray.popStackTwo());
     }
 }
--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -16,6 +16,7 @@ public class DoublyLinkedList {
     private Node tail;
     private int size = 0;
 
+    //Method for adding nodes at the head of the list.
     public void add(int valueToAdd) {
         Node node = new Node(valueToAdd);
 
@@ -34,43 +35,38 @@ public class DoublyLinkedList {
         return size == 0;
     }
 
+    //Method to compare the two lists
     public boolean compareWithReverse(DoublyLinkedList compareNode) {
-        if (compareNode.size != size)
+        if (compareNode.size != size) //If the lists aren't the same size return, since lists cannot be reverse of each other.
             return false;
 
-        while (compareNode.tail != null && head != null && compareNode.tail.value == head.value) {
-            compareNode.tail = compareNode.tail.prev;
-            head = head.next;
+        //If the tail or head is null, it should break out since there is nothing left in the list
+        while (compareNode.tail != null && head != null) {
+            if (compareNode.tail.value == head.value) {
+                //Set the head to the next value, and the tail to the previous.
+                compareNode.tail = compareNode.tail.prev;
+                head = head.next;
+            } else
+                return false;
         }
-
         return true;
     }
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime(); //Start timer
+
+        //Set list values
         DoublyLinkedList list = new DoublyLinkedList();
-
-//        for (int i = 0; i<1000000; i++)
-//            list.add(i);
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        list.add(1);list.add(2);list.add(3);
+        list.add(6);list.add(5);list.add(9);
+        list.add(18);list.add(2);
 
         DoublyLinkedList reverseList = new DoublyLinkedList();
+        reverseList.add(2);reverseList.add(18);reverseList.add(9);
+        reverseList.add(5);reverseList.add(6);reverseList.add(3);
+        reverseList.add(2);reverseList.add(1);
 
-//        for (int i = 999999; i>-1; i--)
-//            reverseList.add(i);
-
-        reverseList.add(5);
-        reverseList.add(4);
-        reverseList.add(3);
-        reverseList.add(2);
-        reverseList.add(1);
-
-        long startTime = System.nanoTime();
-
+        //Compare
         if (list.compareWithReverse(reverseList))
             System.out.println("Is Match");
         else
@@ -79,6 +75,6 @@ public class DoublyLinkedList {
         long stopTime = System.nanoTime();
 
         long microSeconds = TimeUnit.NANOSECONDS.toMicros(stopTime - startTime);
-        System.out.println(microSeconds + " Micro seconds.");
+        System.out.println(microSeconds + " Micro seconds."); //Display time
     }
 }
